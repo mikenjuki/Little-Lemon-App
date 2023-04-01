f9b2535032cfe76e1733f2020913fed99e675680
Michael Njuki <mikenjuki@gmail.com>
2023-04-01 11:05:32 +0300

    Created and styled the testimonials secton

---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles";
+
+const TestimonialCard = ({ id, content, name, img }) => (
+  <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-LemonBeige md:w-1/3 my-8 mx-4">
+    <img src={img} alt={`${id}`} className="w-16 -mt-14" />
+    <h5 className="text-lg font-bold font-Karla">{name}</h5>
+    <p className="text-sm text-LemonDark font-Karla">{content}</p>
+  </div>
+);
+
+export default TestimonialCard;
