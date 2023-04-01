2e3e84a205f78c0718eb04efefb9b0bc487e1429
Michael Njuki <mikenjuki@gmail.com>
2023-04-01 12:23:17 +0300

    created and styled the CTA component

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Button from "./Button";
 
-const CTA = () => {
-  return <div>CTA</div>;
-};
+const CTA = () => (
+  <section id="cta" className="bg-LemonBeige">
+    <div className="container flex flex-col items-center justify-between px-6 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0">
+      <h2 className="text-5xl font-bold leading-tight text-center text-LemonGreen md:text-4xl md:max-w-xl md:text-left">
+      Indulge your taste buds today!
+      </h2>
+
+      <div className="my-16">
+       <Button>Book Reservation</Button>
+      </div>
+    </div>
+  </section>
+);
 
 export default CTA;
